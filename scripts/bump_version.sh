978116dac72a7da892291cbd86ed8148abfa6007
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2023-06-05 23:18:05 -0400

    fixed bug when version is missing

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -3,6 +3,8 @@
 git --version
 git remote remove origin
 git remote add origin https://${GITLAB_USER_LOGIN}:${ACCESS_TOKEN}@${CI_SERVER_NAME}/${GITLAB_USER_LOGIN}/${CI_PROJECT_PATH}
+git config user.email "gitlab-runner@linuxbuild"
+git config user.name "gitlab runner"
 cur_version=$(git describe --tags)
 if [[ -z ${cur_version} ]]
 then
