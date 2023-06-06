3f87a972c32725dbc1dd144c69215da4508b08cd
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2023-06-05 23:25:25 -0400

    fixing gitlab server name

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -4,7 +4,7 @@ export
 
 git --version
 git remote remove origin
-git remote add origin https://${GITLAB_USER_LOGIN}:${ACCESS_TOKEN}@${CI_SERVER_NAME}/${CI_PROJECT_PATH}
+git remote add origin https://${GITLAB_USER_LOGIN}:${ACCESS_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}
 git config user.email "gitlab-runner@linuxbuild"
 git config user.name "gitlab runner"
 cur_version=$(git describe --tags)
