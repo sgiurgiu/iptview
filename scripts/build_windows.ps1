d9e2494f237d60fe4b951eaa8d4990eb5d8f087f
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2023-06-07 00:19:04 -0400

    fixing windows build script

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -1,3 +1,10 @@
+param(
+    [string]
+    $libMpvDir = "E:\projects\mpv",
+    [string]
+    $libMpvIncludeDir = "E:\projects\mpv\include",
+)
+
 # Invokes a Cmd.exe shell script and updates the environment.
 function Invoke-CmdScript {
   param(
@@ -24,8 +31,11 @@ Remove-Item -LiteralPath $buildDir -Force -Recurse -ErrorAction Ignore
 Remove-Item -LiteralPath $packagesDir -Force -Recurse -ErrorAction Ignore
 New-Item -ItemType Directory -Force -Path $buildDir
 New-Item -ItemType Directory -Force -Path $packagesDir
+echo "Using lib mpv directory: $libMpvDir"
+echo "Using lib mpv include directory: $libMpvIncludeDir"
 
-cmake -B $buildDir -S . -G Ninja -DLIBMPV_DIR=E:/projects/mpv-dev-x86_64-20210103-git-3e175df -DLIBMPV_INCLUDE=E:/projects/mpv-dev-x86_64-20210103-git-3e175df/include -DCMAKE_BUILD_TYPE=Release -DVCPKG_INSTALL_OPTIONS="--x-buildtrees-root=E:/sa"  -DCMAKE_TOOLCHAIN_FILE=E:/projects/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCPACK_GENERATOR=WIX
+
+cmake -B $buildDir -S . -G Ninja -DLIBMPV_DIR="$libMpvDir" -DLIBMPV_INCLUDE="$libMpvIncludeDir" -DCMAKE_BUILD_TYPE=Release -DVCPKG_INSTALL_OPTIONS="--x-buildtrees-root=E:/sa"  -DCMAKE_TOOLCHAIN_FILE=E:/projects/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCPACK_GENERATOR=WIX
 
 cmake --build $buildDir -- -j4 package
 
